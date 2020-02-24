398357dbff1b93c2e2235de858feafde4d516d58
Gabriel Deschesnes <1481421@cshawi.ca>
2020-02-24 13:04:41 -0500

    1er commit

---
+++ wpf_intra/intra_models/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace intra_models
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string PostalCode { get; set; }
+        public string ContactInfo { get; set; }
+        public string PicturePath { get; set; }
+        public string Info { get; set; }
+
+    }
+}
